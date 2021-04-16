48071cf3c53103e9ef47df17f1f8f6f5055669c9
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-04-16 13:55:36 +0200

    Displaying a list with currencies

---
+++ src/components/CurrenciesItem/CurrenciesItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from "./CurrenciesItem.module.css";
+
+const currenciesItem = (props) => {
+  return (
+    <ul className={classes.currenciesItemWrapper}>
+      <li>{props.obj.currency}</li>
+      <li>{props.obj.code}</li>
+      <li className={classes.center}>{props.obj.mid}</li>
+      <button className={classes.followButton}>Obserwuj</button>
+    </ul>
+  );
+};
+
+export default currenciesItem;
