7d133f3e98db3523362f9c5074e9d3faf0e7f621
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-04-18 19:07:46 +0200

    Adding a spinner before the page is loaded

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ import Layout from "./hoc/Layout/Layout";
 import Currencies from "./containers/Currencies/Currencies";
 
 class App extends Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.demoAsyncCall().then(() => this.setState({ loading: false }));
+  }
+
+  demoAsyncCall() {
+    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+  }
   render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      return null;
+    }
     let routes = (
       <Switch>
         <Route path="/" exact>
